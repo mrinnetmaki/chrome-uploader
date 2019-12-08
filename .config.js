e3da0fb0bf4f0ffd0e5e627a516d3d6438c80a3c
Mikael Rinnetmäki <mikael@sensotrend.com>
2019-12-08 12:51:48 +0200

    Use European time zone

--- .config.js
+++ .config.js
@@ -41,6 +41,6 @@ module.exports = {
   UPLOAD_URL: process.env.UPLOAD_URL || 'https://test.nightscout.fi/tpupload',
   DATA_URL: process.env.DATA_URL || 'https://test.nightscout.fi/tpdata',
   BLIP_URL: process.env.BLIP_URL || 'http://sensotrend.fi/dashboard/',
-  DEFAULT_TIMEZONE: process.env.DEFAULT_TIMEZONE || 'America/Los_Angeles',
+  DEFAULT_TIMEZONE: process.env.DEFAULT_TIMEZONE || 'Europe/Helsinki',
   DEFAULT_CARELINK_DAYS: process.env.DEFAULT_CARELINK_DAYS || '180'
 };
