31fd954442cce4a785351219666596503599666a
Jyrki Launonen <jyrki.launonen@vincit.fi>
2021-01-13 15:22:50 +0200

    Change Blip URL to point to better location.

--- app/containers/App.js
+++ app/containers/App.js
@@ -67,31 +67,31 @@ const serverdata = {
     API_URL: 'http://localhost:1300/tpapi',
     UPLOAD_URL: 'http://localhost:1300/tpupload',
     DATA_URL: 'http://localhost:1300/tpdata',
-    BLIP_URL: 'http://localhost:1300/view'
+    BLIP_URL: 'http://localhost:1300/api'
   },
   Development: {
     API_URL: 'https://dev-connect.sensotrend.fi/tpapi',
     UPLOAD_URL: 'https://dev-connect.sensotrend.fi/tpupload',
     DATA_URL: 'https://dev-connect.sensotrend.fi/tpdata',
-    BLIP_URL: 'https://dev-connect.sensotrend.fi/view'
+    BLIP_URL: 'https://dev.sensotrend.fi/api'
   },
   Staging: {
     API_URL: 'https://test-connect.sensotrend.fi/tpapi',
     UPLOAD_URL: 'https://test-connect.sensotrend.fi/tpupload',
     DATA_URL: 'https://test-connect.sensotrend.fi/tpdata',
-    BLIP_URL: 'https://test-connect.sensotrend.fi/view'
+    BLIP_URL: 'https://test.sensotrend.fi/api'
   },
   Integration: {
     API_URL: 'https://test-connect.sensotrend.fi/tpapi',
     UPLOAD_URL: 'https://test-connect.sensotrend.fi/tpupload',
     DATA_URL: 'https://test-connect.sensotrend.fi/tpdata',
-    BLIP_URL: 'https://test-connect.sensotrend.fi/view'
+    BLIP_URL: 'https://test.sensotrend.fi/api'
   },
   Production: {
     API_URL: 'https://connect.sensotrend.fi/tpapi',
     UPLOAD_URL: 'https://connect.sensotrend.fi/tpupload',
     DATA_URL: 'https://connect.sensotrend.fi/tpdata',
-    BLIP_URL: 'https://connect.sensotrend.fi/view'
+    BLIP_URL: 'https://www.sensotrend.fi/api'
   }
 };
 
